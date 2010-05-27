3bf20c2d1071460476d01e71203fb73941a067d3
Kevin Q <qu-chunguang@necsoft.com.cn>
2010-05-27 17:28:42 +0800

    add testpy/

---
+++ testpy/callfromshell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 'Run python from shell'
+python <<'END'
+#!/usr/bin/python
+import subprocess
+
+subprocess.call("ls -l $PWD/", shell=True)
+subprocess.call(["ls", "-l", "/home/qcg/"])
+subprocess.call("for i in 1 2 3; do echo $i; done", shell=True)
+END
+
