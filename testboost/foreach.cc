65b57cbfdc821fd450275d0c5a46a0bbc6c9a775
Kevin Q <quchunguang@gmail.com>
2010-11-04 21:41:24 +0800

    add testboost/foreach.cc

---
+++ testboost/foreach.cc
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <boost/foreach.hpp>
+
+int main()
+{
+	std::string hello( "Hello, world!" );
+
+	BOOST_FOREACH( char ch, hello )
+	{
+		std::cout << ch;
+	}
+
+	return 0;
+}
