f3231de61fdad76c54480d008bfe70f3014087a2
Kevin Q <quchunguang@gmail.com>
2011-06-29 23:05:41 +0800

    link data to object by objcopy

--- testother/testuid.c
+++ testother/testuid.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
+
+/* getuid() geteuid() */
 #include <unistd.h>
 #include <sys/types.h>
 
+/* objcopy from linkdata.jpg */
+extern char _binary_linkdata_jpg_start[];
+extern char _binary_linkdata_jpg_end[];
+extern char _binary_linkdata_jpg_size[];
+const char *start = _binary_linkdata_jpg_start;
+const char *end = _binary_linkdata_jpg_end;
+int size = (int)_binary_linkdata_jpg_size;
+
+/* foo.so */
+extern void foo();
+extern void bar();
+
 int main()
 {
+	char* str = "Hello World\n";
 	printf("uid = %d, euid = %d ", getuid(), geteuid());
-	printf("\n");
+	printf("%s",str);
+
+	/* call methods in foo.so */
+	foo();
+	bar();
+
+	/* recall data imported by objcopy */
+	printf("Size of linkdata.jpg=%d\n", size);
 	return 0;
 }
