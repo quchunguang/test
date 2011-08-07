9d509fa9d717f09ca54fb2cfdc6cd55e832e3cf6
quchunguang <quchunguang@gmail.com>
2011-08-07 13:56:15 +0800

    little

--- sams_server/ensure.php
+++ sams_server/ensure.php
@@ -11,7 +11,7 @@ $sql = "select ukey_id,random,status from customer where product_id='" . $_POST[
 $result = mysql_query($sql, $db);
 if ($myrow = mysql_fetch_array($result)) {
 	if ($myrow["status"] == 1){
-		exec("/var/www/sams/senseCrypt '" . $_POST["encrypt"] . "' '" . $myrow["ukey_id"] . "' '" . $myrow["random"] . "'", $lines, $ret);
+		exec("d:\\qcg\\PHPnow\\sams_server\\senseCrypt.exe '" . $_POST["encrypt"] . "' '" . $myrow["ukey_id"] . "' '" . $myrow["random"] . "'", $lines, $ret);
 		if ($ret == 0) {
 			$sql = "update customer set status=2 where product_id='" . $_POST["product_id"] . "'";
 			$result = mysql_query($sql, $db);
