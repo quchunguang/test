5b045de3a5a1d2eb1603eec7ccd1d461ba3217be
Kevin Q <quchunguang@gmail.com>
2011-08-22 11:08:20 +0800

    sams_server/geturl.php add version

--- sams_server/geturl.php
+++ sams_server/geturl.php
@@ -7,7 +7,7 @@ if (!$db) {
 }
 
 mysql_select_db("sams", $db);
-$sql = "select status,functions,random from customer where product_id='" . $_POST["product_id"] . "'";
+$sql = "select status,functions,random,revision,encrypt_limit from customer where product_id='" . $_POST["product_id"] . "'";
 $result = mysql_query($sql, $db);
 if ($myrow = mysql_fetch_array($result)) {
 	if ($myrow["status"] == 2){
@@ -18,15 +18,31 @@ if ($myrow = mysql_fetch_array($result)) {
 		}
 		$installer = "installer.exe";
 		$url_installer = "download/" . $installer;
-		echo "{'code':0,'url_installer':'" . $url_installer . "','functions':";
+		echo "{'code':0,'url_installer':'" . $url_installer . "'";
+
+		# generate limit and encrypt_limit
+		echo ",'encrypt_limit':'" . $myrow["encrypt_limit"] . "'";
+
+		# generate sql list as sqls[]
+		$sql = "select alter_sql from version where revision > " . $myrow["revision"] . " and alter_sql is not null order by revision asc;";
+		$result3 = mysql_query($sql, $db);
+		$sqls = array();
+		while($r3 = mysql_fetch_assoc($result3)) {
+			$sqls[] = $r3;
+		}
+		echo ",'sqls':";
+		echo json_encode($sqls);
+
+		# generate function list
 		$sql = "SET CHARACTER_SET_RESULTS=utf8";
-		$result = mysql_query($sql, $db);
+		$result2 = mysql_query($sql, $db);
 		$sql = "select a1,a2,bh,parent,a5,a6,a7,level,isLeaf,showindex,a8 from functions where id in (". $myrow["functions"] .")";
-		$result = mysql_query($sql, $db);
+		$result2 = mysql_query($sql, $db);
 		$rows = array();
-		while($r = mysql_fetch_assoc($result)) {
+		while($r = mysql_fetch_assoc($result2)) {
 			$rows[] = $r;
 		}
+		echo ",'functions':";
 		echo json_encode($rows);
 		echo "}";
 	} else {
