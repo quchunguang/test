2fc9f788bdb0b0e8a6cf47dd63de0646448a2672
quchunguang <quchunguang@gmail.com>
2011-08-25 17:34:29 +0800

    sams_server/changelimit.php fix bug sams_server/changelimit_test.html added

--- sams_server/changelimit.php
+++ sams_server/changelimit.php
@@ -6,6 +6,11 @@ if (!$db) {
 	die('Could not connect: ' . mysql_error());
 }
 
+if($_POST["type"] != "u500" && $_POST["type"] != "u1000" && $_POST["type"] != "unlimit") {
+	echo "{'code':3}"; # type error
+	die();
+}
+
 mysql_select_db("sams", $db);
 $sql = "select ukey_id from customer where product_id='" . $_POST["product_id"] . "'";
 $result = mysql_query($sql, $db);
@@ -16,15 +21,16 @@ if ($myrow = mysql_fetch_array($result)) {
 	if ($myrow2 = mysql_fetch_array($result)) {
 		exec("d:\\qcg\\PHPnow\\htdocs\\sams_server\\senseCryptBase64.exe 0 '" . $myrow["ukey_id"] . " " . $_POST["product_id"] . " 36", $lines, $ret);
 		if ($ret == 0) {
-			$sql = "update customer set crypt_limit='" . $lines[0] . "' where product_id='" . $_POST["product_id"] . "'";
+			$sql = "update customer set encrypt_limit='" . $lines[0] . "' where product_id='" . $_POST["product_id"] . "'";
 			$result = mysql_query($sql, $db);
 			if(!$result) {
 				echo "{'code':6}"; # update status error
+				die();
 			}
 		}
 		echo "{'code':" . $ret . "}";
 	} else {
-		echo "{'code':2}"; # type error
+		echo "{'code':2}"; # revision error
 	}
 } else {
 	echo "{'code':1}"; # no such product
