e4adde23b5754cfc46481bacff6590ec4e2e83db
Kevin Qu <quchunguang@gmail.com>
2012-12-16 16:58:08 +0800

    testpy/testcurses.py

---
+++ testpy/testcurses.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+
+import curses
+
+myscreen = curses.initscr()
+myscreen.border(0)
+myscreen.addstr(12, 25, "Python curses in action!")
+myscreen.refresh()
+myscreen.getch()
+curses.endwin()
