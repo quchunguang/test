affdbb5b7fabf6084bde7053f1ed98ae1892053e
Qu chunguang <quchunguang@gmail.com>
2015-04-07 22:13:55 +0800

    little

--- test51/main.c
+++ test51/main.c
@@ -1,6 +1,20 @@
 #include <8052.h>
+#define NOP __asm \
+nop \
+__endasm
+
+#define THREENOPS NOP; \
+NOP; \
+NOP
 
 void main(void)
 {
-    P1 = 0xff;
+    int n = 500;
+    P1 = 0x02;
+    while(1){
+        while(n--);
+        n = 10000;
+        P1 = ~P1;
+        THREENOPS;
+    }
 }
