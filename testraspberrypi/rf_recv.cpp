40e32cbc888bdf42675903549372100cffe18d94
Kevin Qu <quchunguang@gmail.com>
2015-04-29 18:42:36 +0800

    little

--- testraspberrypi/rf_recv.cpp
+++ testraspberrypi/rf_recv.cpp
@@ -5,22 +5,10 @@
 #include <RF24/RF24.h>
 
 using namespace std;
-// Hardware configuration
 // Radio CE Pin, CSN Pin, SPI Speed
 // See http://www.airspayce.com/mikem/bcm2835/group__constants.html#ga63c029bd6500167152db4e57736d0939 and the related enumerations for pin information.
-// Setup for GPIO 22 CE and CE0 CSN with SPI Speed @ 4Mhz
-//RF24 radio(RPI_V2_GPIO_P1_22, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_4MHZ);
-// NEW: Setup for RPi B+
-//RF24 radio(RPI_BPLUS_GPIO_J8_15,RPI_BPLUS_GPIO_J8_24, BCM2835_SPI_SPEED_8MHZ);
-// Setup for GPIO 15 CE and CE0 CSN with SPI Speed @ 8Mhz
-RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_24);
-/*** RPi Alternate ***/
-//Note: Specify SPI BUS 0 or 1 instead of CS pin number.
-// See http://tmrh20.github.io/RF24/RPi.html for more information on usage
-//RPi Alternate, with MRAA
-//RF24 radio(15,0);
-//RPi Alternate, with SPIDEV - Note: Edit RF24/arch/BBB/spi.cpp and  set 'this->device = "/dev/spidev0.0";;' or as listed in /dev
-//RF24 radio(22,0);
+// Setup for RPi 2
+RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
 /****************** Linux (BBB,x86,etc) ***********************/
 // See http://tmrh20.github.io/RF24/pages.html for more information on usage
 // See http://iotdk.intel.com/docs/master/mraa/ for more information on MRAA
@@ -30,7 +18,7 @@ RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_24);
 // CE pin = (Header P9, Pin 13) = 59 = 13 + 46
 //Note: Specify SPI BUS 0 or 1 instead of CS pin number.
 //RF24 radio(59,0);
-/**************************************************************/
+
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 const int min_payload_size = 4;
