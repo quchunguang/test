a5a80ddd8b2514679c253577bda37bafce20b056
Kevin Qu <quchunguang@gmail.com>
2015-05-07 21:40:15 +0800

    testpy3/pyqt5 tutorial

---
+++ testpy3/pyqt5_event.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QWidget, QPushButton,QLCDNumber, QSlider,QHBoxLayout,
+                             QVBoxLayout, QApplication)
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        lcd = QLCDNumber(self)
+        sld = QSlider(Qt.Horizontal, self)
+        sld.valueChanged.connect(lcd.display)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lcd)
+        vbox.addWidget(sld)
+
+        btn1 = QPushButton("Button 1", self)
+        btn2 = QPushButton("Button 2", self)
+        btn1.clicked.connect(self.buttonClicked)
+        btn2.clicked.connect(self.buttonClicked)
+        hbox = QHBoxLayout()
+        hbox.addWidget(btn1)
+        hbox.addWidget(btn2)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+        self.setGeometry(300, 300, 250, 250)
+        self.setWindowTitle('Signal & slot')
+        self.show()
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
+
+    def buttonClicked(self):
+        sender = self.sender()
+        print(sender.text() + ' was pressed')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
---
+++ testpy3/pyqt5_statusbar.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, qApp, QApplication
+from PyQt5.QtGui import QIcon
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+
+    def initUI(self):
+        # textEdit
+        textEdit = QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        # action
+        exitAction = QAction(QIcon('web.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        # menu
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        # tool bar
+        exitToolBar = self.addToolBar('Exit')
+        exitToolBar.addAction(exitAction)
+        commonToolBar = self.addToolBar('Common')
+        commonToolBar.addAction(exitAction)
+
+        # status bar
+        self.statusBar().showMessage('Ready')
+
+        # main window
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Main Window')
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
